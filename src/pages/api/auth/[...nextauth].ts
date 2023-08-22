8df17516794e5eb3783aa87e66d145f769853eeb
Max Timje <max@timje.se>
2023-08-22 09:52:51 +0200

    update nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,47 +1,21 @@
-// import { prisma } from "@/pages/lib/prisma";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-
-// export const authOptions = {
-//   adapter: PrismaAdapter(prisma),
-//   // Configure one or more authentication providers
-//   providers: [
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID as string,
-//       clientSecret: process.env.GITHUB_SECRET as string,
-//     }),
-//     // ...add more providers here
-//   ],
-// };
-// export default NextAuth(authOptions);
-
 import NextAuth from "next-auth";
-// import Providers from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-    // ...add more providers here
   ],
-  adapter: PrismaAdapter(prisma),
 });
